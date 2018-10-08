d5d8349053fe6bc8ba9861920cec2ed2657a6407
Keanu Kerr <keanukerr@gmail.com>
2018-10-08 15:56:45 +0100

    Divides occurances by total trigram count

--- src/Trigram.py
+++ src/Trigram.py
@@ -33,27 +33,34 @@ class Trigram():
     # beginning and end of each line. Depending on how you write the rest of
     # your program, you may need to modify this code.
     def extractTrigram(self):
+        print("Extracting trigrams")
+        total_tri_counts = 0
         with open(self.infile) as f:
             for line in f:
                 line = self.preprocess_line(line)
                 for j in range(len(line)-(3)):
                     trigram = line[j:j+3]
                     self.tri_counts[trigram] += 1
+                    total_tri_counts += 1
+        self.printTrigram(total_tri_counts)
 
     # Some example code that prints out the counts. For small input files
     # the counts are easy to look at but for larger files you can redirect
     # to an output file (see Lab 1).
-    def printTrigram(self):
+    def printTrigram(self, total_tri_counts):
+        print('total trigram count: ', total_tri_counts)
         print("Trigram counts in ", self.infile, ", sorted alphabetically:")
         with open('alphabetical_trigram.txt', 'w') as f:
             for trigram in sorted(self.tri_counts.keys()):
                 # print(trigram, ": ", self.tri_counts[trigram])
-                print(trigram, ": ", self.tri_counts[trigram], file=f)
+                print(trigram, ": ",
+                      '{:.2e}'.format(self.tri_counts[trigram] / total_tri_counts), file=f)
         print("Trigram counts in ", self.infile, ", sorted numerically:")
         with open('numerical_trigram.txt', 'w') as f:
             for tri_count in sorted(self.tri_counts.items(), key=lambda x: x[1], reverse=True):
                 # print(tri_count[0], ": ", str(tri_count[1]))
-                print(tri_count[0], ": ", str(tri_count[1]), file=f)
+                print(tri_count[0], ": ", str(
+                    '{:.2e}'.format(tri_count[1] / total_tri_counts)), file=f)
 
     # Task 4
     # Generates output string based on language model
@@ -63,7 +70,6 @@ class Trigram():
 
 def main():
     Trigram().extractTrigram()
-    Trigram().printTrigram()
 
 
 if __name__ == "__main__":
